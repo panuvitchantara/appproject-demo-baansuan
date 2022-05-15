7dd168f3c151be46e7aee76c9a80b78dda0dd758
Panuvit Chantara <panuvit.chantara@ait.asia>
2022-05-15 11:42:39 +0700

    change layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import BlogGallery from './components/blog-gallery/blog-gallery.component';
 import NavBar from './components/navigation/navigation.component';
 import SlideShow from './components/slideshow/slide.component';
 import Footer from './components/footer/footer.component'
+import Paper from '@mui/material/Paper';
 
 const App = () => {
 
@@ -11,11 +12,21 @@ const App = () => {
     <div className="App">
       <NavBar />
       <SlideShow />
+
+      <div className="activities-showcase-container">
+        <div className="activities-showcase-header">
+          <h1>กิจกรรมของเรา</h1>
+        </div>
+        <div className="activities-showcase-gallery-container">
+          <Paper className="paper-container" elevation={3} >Test</Paper>
+          <Paper elevation={3} >Test</Paper>
+          <Paper elevation={3} >Test</Paper>
+        </div>
+      </div>
+
       <BlogGallery />
+
       <Footer />
-      
-
-
     </div>
   );
 }
